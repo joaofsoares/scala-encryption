a993912bc3da55a06facf9dfb172c9aef8a68593
João Soares <joao.soares@stctecnologia.com.br>
2017-06-26 09:20:26 -0300

    final implement

--- src/main/scala-2.12/com/github/encryption/ScalaEncryption.scala
+++ src/main/scala-2.12/com/github/encryption/ScalaEncryption.scala
@@ -13,18 +13,24 @@ import org.apache.commons.codec.binary.Base64
   */
 class ScalaEncryption(secretWord: String) {
 
-  private val keySpec = new SecretKeySpec(secretWord.getBytes("UTF-8"), "AES")
-  private val iv = new IvParameterSpec(secretWord.getBytes("UTF-8"))
-  private val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+  private final val keySpec = new SecretKeySpec(secretWord.getBytes("UTF-8"), "AES")
+  private final val iv = new IvParameterSpec(secretWord.getBytes("UTF-8"))
+  private final val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
 
-  def encrypt(value: String): String = {
+  final def encrypt(value: String): String = {
+
     cipher.init(Cipher.ENCRYPT_MODE, keySpec, iv)
+
     Base64.encodeBase64String(cipher.doFinal(value.getBytes()))
+
   }
 
-  def decrypt(value: String): String = {
-    cipher.init(Cipher.DECRYPT_MODE, keySpec, iv);
-    new String(cipher.doFinal(Base64.decodeBase64(value)));
+  final def decrypt(value: String): String = {
+
+    cipher.init(Cipher.DECRYPT_MODE, keySpec, iv)
+
+    new String(cipher.doFinal(Base64.decodeBase64(value)))
+
   }
 
 }
